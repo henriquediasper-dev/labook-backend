5685ad8541025e438bec9070aca4397afb967b69
henriquedias420 <henriquediasper@hotmail.com>
2023-08-04 11:27:08 -0300

    [IMPLEMENTAÇÂO] DTOs

---
+++ src/dtos/post/likeOrDislikePost.dto.ts
@@ -0,0 +1,17 @@
+import z from "zod";
+
+export interface LikeOrDislikePostInputDTO {
+  idToLikeOrDislike: string;
+  token: string;
+  like: boolean;
+}
+
+export type LikeOrDislikePostsOutputDTO = undefined;
+
+export const GetPostsSchema = z
+  .object({
+    idToLikeOrDislike: z.string().min(1),
+    token: z.string().min(1),
+    like: z.boolean(),
+  })
+  .transform((data) => data as LikeOrDislikePostInputDTO);
